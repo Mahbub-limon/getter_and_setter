2bab52e142b0b10b8ff2aec0cf57637d8ff037a6
Mahbub-limon <mahbublimon1127@gmail.com>
2024-10-15 22:57:28 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readWrite
+{
+    class Person 
+    {
+        public string Name;
+
+        private int age;
+
+        public void SetAge(int age) 
+        {
+            this.age = age;
+        }
+    
+        public int Getage() 
+        {
+            return age;
+        }
+
+        
+
+    public void DisplayInfo()
+        {
+            Console.WriteLine($"Name:{Name}");
+        }
+    
+    
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "limon";
+            person.SetAge(23);
+            person.Getage();
+            person.DisplayInfo();
+            Console.WriteLine($"Age:{person.Getage()}");
+            
+        }
+    }
+}
